c56f34edd801a88a95e5760fe9dab590dce8ece4
Mateusz Ostrowski <niewerbalnie@gmail.com>
2021-03-29 20:21:47 +0100

    Data Service implemented

--- Client/src/app/shared/data/data.service.ts
+++ Client/src/app/shared/data/data.service.ts
@@ -2,9 +2,70 @@ import { Injectable } from "@angular/core";
 import { DayInfo } from "Models/DayInfo";
 import { Subject } from "rxjs";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class DataService {
-    //?
-    newDayInputEmmiter = new Subject<DayInfo>();
+    date: Date = new Date();
+        /*newDayInfo: DayInfo = {
+            TimeOfStart: 0,
+            TimeOfFinish: 720,
+            TimeOfStart2: 900,
+            TimeOfFinish2: 1260,
+            AddAfternoonTime: false,
+            Day: this.date.getDate(),
+            DayOfWeek: this.date.getDay(),
+            Month: (this.date.getMonth() + 1),
+            Year: this.date.getFullYear(),
+            getDate: DayInfo.prototype.getDate,
+            getDayOfWeekInPolish: DayInfo.prototype.getDayOfWeekInPolish
+        }; */
+    newDayInputEmmiter: Subject<DayInfo> = new Subject<DayInfo>();
+
+    newDayInfo: DayInfo = new DayInfo();
+
+    updateNewDayTimes(TimeOfStart?: number, TimeOfFinish?: number, TimeOfStart2?: number, TimeOfFinish2?: number) {
+        if (TimeOfStart) {
+            this.newDayInfo.TimeOfStart = TimeOfStart;
+        }
+        if (TimeOfFinish) {
+            this.newDayInfo.TimeOfFinish = TimeOfFinish;
+        }
+        if (TimeOfStart2) {
+            this.newDayInfo.TimeOfStart2 = TimeOfStart2;
+        }
+        if (TimeOfFinish2) {
+            this.newDayInfo.TimeOfFinish2 = TimeOfFinish2;
+        }
+    }
+    updateNewDayDates(Day: number, DayOfWeek: number, Month: number, Year: number){
+        if (Day){
+            this.newDayInfo.Day = Day;
+        }
+        if (DayOfWeek){
+            this.newDayInfo.DayOfWeek = DayOfWeek;
+        }
+        if (Month){
+            this.newDayInfo.Month = Month;
+        }
+        if (Year){
+            this.newDayInfo.Year = Year;
+        }
+    }
+    resetNewDayTime(){
+        this.newDayInfo.TimeOfStart = 0;
+        this.newDayInfo.TimeOfFinish = 720;
+        this.newDayInfo.TimeOfStart2 = 900;
+        this.newDayInfo.TimeOfFinish2 = 1260;
+        this.newDayInfo.AddAfternoonTime = false;
+    }
+    setTodayDate(){
+        this.newDayInfo.Day = this.date.getDate();
+        this.newDayInfo.DayOfWeek = this.date.getDay();
+        this.newDayInfo.Month = (this.date.getMonth() + 1);
+        this.newDayInfo.Year = this.date.getFullYear();
+    }
+    getNewDayInfo(){
+        return this.newDayInfo;
+    }
+
 }
