1fcba743f2a0ebf875ccf41293983f189b52f91f
Mateusz Ostrowski <niewerbalnie@gmail.com>
2021-04-09 18:32:05 +0100

    AddedFirebaseToolsAndSomeFixes

--- Client/functions/src/index.ts
+++ Client/functions/src/index.ts
@@ -2,10 +2,14 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from ADMIN!");
+export const onUserSignup = functions.auth.user().onCreate((user) => {
+    const pathToUserPrivateInfoDoc = admin.firestore()
+        .collection("users")
+        .doc(user.uid)
+        .collection("userInfo")
+        .doc("userPrivateInfo");
+
+    return pathToUserPrivateInfoDoc.set({
+        email: user.email,
+    });
 });
