24cd8e0cc2dbb73825e9894dee7905b78c6d1955
Mateusz Ostrowski <niewerbalnie@gmail.com>
2021-04-10 17:32:03 +0100

    CleaningBackend2

--- TruckCalculatorAppAPI/Models/User.cs
+++ TruckCalculatorAppAPI/Models/User.cs
@@ -6,27 +6,6 @@ using System.Threading.Tasks;
 
 namespace TruckCalculatorAppAPI.Models
 {
-    // export class User {
-    //     public email: string;
-    //     public id: string;
-    //     public _token: string;
-    //     public _tokenExpirationDate: Date;
-    //     constructor() { 
-    //         this.email = "notValid",
-    //         this.id = "notValid",
-    //         this._token = "notValid",
-    //         this._tokenExpirationDate = new Date();
-    //     }
-    //     get token() {
-    //         if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
-    //             return null;
-    //         }
-    //         else {
-    //             return this._token;
-    //         }
-    //     }
-    // }
-
     public class User
     {
         public string Email { get; set; }
